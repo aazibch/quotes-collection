b583f59bf61f68a51640d0839417ed17871cc814
Aazib Chaudhry <aazibch47@gmail.com>
2023-03-21 00:21:55 +0500

    Update package

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose
-    .connect(process.env.DB.replace('<password>', process.env.DB_PASSWORD))
-    .then(() => console.log('Connected to database.'));
+  .connect(process.env.DB.replace('<password>', process.env.DB_PASSWORD))
+  .then(() => console.log('Connected to database.'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
+  console.log(`App running on port ${port}.`);
 });
